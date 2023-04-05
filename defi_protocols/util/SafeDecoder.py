f8c97bb5e508e39f27cb2fd2df63f297686d0547
sgzerbo <98768518+sgzerbo@users.noreply.github.com>
2023-04-05 18:21:03 -0300

    Progress

--- defi_protocols/util/SafeDecoder.py
+++ defi_protocols/util/SafeDecoder.py
@@ -1,15 +1,15 @@
 from defi_protocols.functions import *
 from defi_protocols.util.api import RequestFromScan
 
+SAFE_CONTRACT_ABI = '[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"owner","type":"address"}],"name":"AddedOwner","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"approvedHash","type":"bytes32"},{"indexed":true,"internalType":"address","name":"owner","type":"address"}],"name":"ApproveHash","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"handler","type":"address"}],"name":"ChangedFallbackHandler","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"guard","type":"address"}],"name":"ChangedGuard","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"threshold","type":"uint256"}],"name":"ChangedThreshold","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"module","type":"address"}],"name":"DisabledModule","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"module","type":"address"}],"name":"EnabledModule","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"bytes32","name":"txHash","type":"bytes32"},{"indexed":false,"internalType":"uint256","name":"payment","type":"uint256"}],"name":"ExecutionFailure","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"module","type":"address"}],"name":"ExecutionFromModuleFailure","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"module","type":"address"}],"name":"ExecutionFromModuleSuccess","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"bytes32","name":"txHash","type":"bytes32"},{"indexed":false,"internalType":"uint256","name":"payment","type":"uint256"}],"name":"ExecutionSuccess","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"owner","type":"address"}],"name":"RemovedOwner","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"SafeReceived","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"initiator","type":"address"},{"indexed":false,"internalType":"address[]","name":"owners","type":"address[]"},{"indexed":false,"internalType":"uint256","name":"threshold","type":"uint256"},{"indexed":false,"internalType":"address","name":"initializer","type":"address"},{"indexed":false,"internalType":"address","name":"fallbackHandler","type":"address"}],"name":"SafeSetup","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"msgHash","type":"bytes32"}],"name":"SignMsg","type":"event"},{"stateMutability":"nonpayable","type":"fallback"},{"inputs":[],"name":"VERSION","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"_threshold","type":"uint256"}],"name":"addOwnerWithThreshold","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"hashToApprove","type":"bytes32"}],"name":"approveHash","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"bytes32","name":"","type":"bytes32"}],"name":"approvedHashes","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_threshold","type":"uint256"}],"name":"changeThreshold","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"dataHash","type":"bytes32"},{"internalType":"bytes","name":"data","type":"bytes"},{"internalType":"bytes","name":"signatures","type":"bytes"},{"internalType":"uint256","name":"requiredSignatures","type":"uint256"}],"name":"checkNSignatures","outputs":[],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"dataHash","type":"bytes32"},{"internalType":"bytes","name":"data","type":"bytes"},{"internalType":"bytes","name":"signatures","type":"bytes"}],"name":"checkSignatures","outputs":[],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"prevModule","type":"address"},{"internalType":"address","name":"module","type":"address"}],"name":"disableModule","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"domainSeparator","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"module","type":"address"}],"name":"enableModule","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"},{"internalType":"enum Enum.Operation","name":"operation","type":"uint8"},{"internalType":"uint256","name":"safeTxGas","type":"uint256"},{"internalType":"uint256","name":"baseGas","type":"uint256"},{"internalType":"uint256","name":"gasPrice","type":"uint256"},{"internalType":"address","name":"gasToken","type":"address"},{"internalType":"address","name":"refundReceiver","type":"address"},{"internalType":"uint256","name":"_nonce","type":"uint256"}],"name":"encodeTransactionData","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"},{"internalType":"enum Enum.Operation","name":"operation","type":"uint8"},{"internalType":"uint256","name":"safeTxGas","type":"uint256"},{"internalType":"uint256","name":"baseGas","type":"uint256"},{"internalType":"uint256","name":"gasPrice","type":"uint256"},{"internalType":"address","name":"gasToken","type":"address"},{"internalType":"address payable","name":"refundReceiver","type":"address"},{"internalType":"bytes","name":"signatures","type":"bytes"}],"name":"execTransaction","outputs":[{"internalType":"bool","name":"success","type":"bool"}],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"},{"internalType":"enum Enum.Operation","name":"operation","type":"uint8"}],"name":"execTransactionFromModule","outputs":[{"internalType":"bool","name":"success","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"},{"internalType":"enum Enum.Operation","name":"operation","type":"uint8"}],"name":"execTransactionFromModuleReturnData","outputs":[{"internalType":"bool","name":"success","type":"bool"},{"internalType":"bytes","name":"returnData","type":"bytes"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getChainId","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"start","type":"address"},{"internalType":"uint256","name":"pageSize","type":"uint256"}],"name":"getModulesPaginated","outputs":[{"internalType":"address[]","name":"array","type":"address[]"},{"internalType":"address","name":"next","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getOwners","outputs":[{"internalType":"address[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"offset","type":"uint256"},{"internalType":"uint256","name":"length","type":"uint256"}],"name":"getStorageAt","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getThreshold","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"},{"internalType":"enum Enum.Operation","name":"operation","type":"uint8"},{"internalType":"uint256","name":"safeTxGas","type":"uint256"},{"internalType":"uint256","name":"baseGas","type":"uint256"},{"internalType":"uint256","name":"gasPrice","type":"uint256"},{"internalType":"address","name":"gasToken","type":"address"},{"internalType":"address","name":"refundReceiver","type":"address"},{"internalType":"uint256","name":"_nonce","type":"uint256"}],"name":"getTransactionHash","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"module","type":"address"}],"name":"isModuleEnabled","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"isOwner","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"nonce","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"prevOwner","type":"address"},{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"_threshold","type":"uint256"}],"name":"removeOwner","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"},{"internalType":"enum Enum.Operation","name":"operation","type":"uint8"}],"name":"requiredTxGas","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"handler","type":"address"}],"name":"setFallbackHandler","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"guard","type":"address"}],"name":"setGuard","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address[]","name":"_owners","type":"address[]"},{"internalType":"uint256","name":"_threshold","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"bytes","name":"data","type":"bytes"},{"internalType":"address","name":"fallbackHandler","type":"address"},{"internalType":"address","name":"paymentToken","type":"address"},{"internalType":"uint256","name":"payment","type":"uint256"},{"internalType":"address payable","name":"paymentReceiver","type":"address"}],"name":"setup","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"name":"signedMessages","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"targetContract","type":"address"},{"internalType":"bytes","name":"calldataPayload","type":"bytes"}],"name":"simulateAndRevert","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"prevOwner","type":"address"},{"internalType":"address","name":"oldOwner","type":"address"},{"internalType":"address","name":"newOwner","type":"address"}],"name":"swapOwner","outputs":[],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive"}]'
 
-def get_safe_functions(tx_hash: str, block: Union[int, str], blockchain: str, web3=None, execution: int = 1,
-                       index: int = 0) -> list:
+def get_safe_functions(tx_hash: str, blockchain: str, web3: object = None, execution: int = 1, index: int = 0) -> list:
     if execution > MAX_EXECUTIONS:
         return None
 
     try:
         if web3 is None:
-            web3 = get_node(blockchain, block=block, index=index)
+            web3 = get_node(blockchain, block=last_block(blockchain), index=index)#We force the use of an archival node with block=last_block(blockchain)
 
         tx_receipt = web3.eth.get_transaction(tx_hash)
         tx_to = tx_receipt['to']
@@ -17,9 +17,17 @@ def get_safe_functions(tx_hash: str, block: Union[int, str], blockchain: str, we
 
         tx_to_impl_code = bytes.fromhex(Web3.toHex(web3.eth.get_storage_at(tx_to, 0))[2:])
         proxy_address = web3.to_checksum_address(tx_to_impl_code[-20:].hex())
-        proxy_address_abi = RequestFromScan(blockchain=blockchain, module='contract', action='getabi',
-                                            kwargs={'address': proxy_address}).request()['result']
-        proxy_contract = web3.eth.contract(address=proxy_address, abi=proxy_address_abi)
+        try:
+            proxy_address_abi = RequestFromScan(blockchain=blockchain, module='contract', action='getabi',kwargs={'address': proxy_address}).request()['result']
+            proxy_contract = web3.eth.contract(address=proxy_address, abi=proxy_address_abi)
+        except:
+            proxy_contract = web3.eth.contract(address=proxy_address, abi=SAFE_CONTRACT_ABI)
+
+        try:
+            proxy_contract.functions.VERSION().call()
+        except:
+            print('To address is not a safe contract')
+            return None
 
         input_data = proxy_contract.decode_function_input(tx_input_data)
         functions, params = input_data
@@ -37,36 +45,38 @@ def get_safe_functions(tx_hash: str, block: Union[int, str], blockchain: str, we
                 function_list = decode_multisend_transaction(transaction, web3)
                 return function_list
             elif 'execTransactionWithRole' in str(functions2):
-                data_output = decode_function_input(params2['to'], params2['data'], blockchain, web3)
-                decode_dict = {'operation': params2['operation'], 'to_address': params2['to'],
-                               'value': params2['value'], 'role': params2['role'], 'data_output': data_output}
+                functions3, params3 = decode_function_input(params2['to'], '0x'+params2['data'].hex(), web3)
+                decode_dict = [{'to_address': params2['to'], 'value': params2['value'], 'signature': str(functions3)[10:-1], 'arguments': params3}]
                 return decode_dict
             else:
-                decode_dict = {'operation': params['operation'], 'to_address': params['to'], 'value': params['value'],
-                               'data_output': input_data_bytes}
+                decode_dict = [{'to_address': params['to'], 'value': params['value'],
+                               'signature': str(functions2)[10:-1], 'arguments': params2}]
                 return decode_dict
         else:
-            print('not a safe transaction')
+            print('Not a safe transaction')
 
     except GetNodeIndexError:
-        return get_safe_functions(tx_hash, block, blockchain, web3, index=0, execution=execution + 1)
+        return get_safe_functions(tx_hash, blockchain, web3, index=0, execution=execution + 1)
 
     except:
-        return get_safe_functions(tx_hash, block, blockchain, web3, index=index + 1, execution=execution)
+        return get_safe_functions(tx_hash, blockchain, web3, index=index + 1, execution=execution)
 
 
 def decode_multisend_transaction(input_data: str, web3) -> list:
-    length_input_data = len(input_data)
+    if web3.eth.chain_id == 1:
+        blockchain = ETHEREUM
+    else:
+        blockchain = XDAI
+
     function_list = []
     for i in range(0, 10):
-        operation = input_data[:2]
+        #operation = input_data[:2]
         to_address = '0x' + input_data[2:42]
         value = input_data[42:106]
         data_length = int('0x' + input_data[106:170].lstrip('0'), 16)
         data_rest = input_data[170:170 + (data_length * 2)]
-        data_output = decode_function_input(to_address, data_rest, blockchain, web3)
-        decode_dict = {'operation': operation, 'to_address': to_address, 'value': value, 'data_length': data_length,
-                       'data_output': data_output}
+        function, params = decode_function_input(to_address, '0x'+data_rest, web3)
+        decode_dict = {'to_address': to_address, 'value': int(value,16), 'signature': str(function)[10:-1], 'arguments': params}
         function_list.append(decode_dict)
         input_data = input_data[170 + (data_length * 2):]
         if len(input_data) == 0:
@@ -74,20 +84,35 @@ def decode_multisend_transaction(input_data: str, web3) -> list:
     return function_list
 
 
-def decode_function_input(function_address: str, input_hex: str, blockchain: str, web3) -> tuple:
-    if function_address[:2] == '0x':
-        checksum_address = web3.to_checksum_address(function_address)
+def decode_function_input(contract_address: str, input_hex: str, web3, abi=None) -> tuple:
+    if contract_address[:2] == '0x':
+        checksum_address = web3.to_checksum_address(contract_address)
     else:
-        checksum_address = web3.to_checksum_address('0x' + function_address)
-    function_abi = RequestFromScan(blockchain=blockchain, module='contract', action='getabi',
-                                   kwargs={'address': checksum_address}).request()['result']
-    function_contract = web3.eth.contract(address=checksum_address, abi=function_abi)
+        checksum_address = web3.to_checksum_address('0x' + contract_address)
+
+    if web3.eth.chain_id == 1:
+        blockchain = ETHEREUM
+    else:
+        blockchain = XDAI
+
+    if abi is None:
+        abi = RequestFromScan(blockchain=blockchain, module='contract', action='getabi', kwargs={'address': checksum_address}).request()['result']
+    function_contract = web3.eth.contract(address=checksum_address, abi=abi)
     function_decode = function_contract.decode_function_input(input_hex)
+
     return function_decode
 
-# blockchain = 'ethereum'
-# tx1 = '0x2971c45416cbf234fe8939599dbb64d5c53210e76e4ff32d89188fa9bea30f87' # multisend
-# tx2 = '0x3c0fbad1350d84a159acef5c3e6fe350e10d44dc894bb3ee6f784b0c167e791f' # exec with role
-# tx3 = '0xd6ef0254c88760e5a2e58924bbaa28f8700341bfa91df469fc9c6f904b732e34' # single call
-# test = get_safe_functions(tx3,'latest',blockchain)
-# print(test)
+web3 = get_node(ETHEREUM)
+#print(decode_function_input('0x074306bc6a6fc1bd02b425dd41d742adf36ca9c6','0x63453ae1000000000000000000000000675ec042325535f6e176638dd2d4994f645502b9', web3))
+
+blockchain = 'ethereum'
+tx1 = '0x2971c45416cbf234fe8939599dbb64d5c53210e76e4ff32d89188fa9bea30f87' # multisend
+tx2 = '0x3c0fbad1350d84a159acef5c3e6fe350e10d44dc894bb3ee6f784b0c167e791f' # exec with role
+tx2_2 = '0x4170bc71b410f331e5d049be7bc4872380134827b69a4e39642c04b918434fa9'# exec with role
+tx3 = '0xd6ef0254c88760e5a2e58924bbaa28f8700341bfa91df469fc9c6f904b732e34' # single call
+print(get_safe_functions(tx1, blockchain))
+print(get_safe_functions(tx2, blockchain))
+print(get_safe_functions(tx2_2, blockchain))
+print(get_safe_functions(tx3, blockchain))
+
+
