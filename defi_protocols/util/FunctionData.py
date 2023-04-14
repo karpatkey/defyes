827eab94ca069dd4a22f94aa054a58398a3576ba
Richard <richymaestro@gmail.com>
2023-04-14 19:01:47 +0100

    more robust data input

--- defi_protocols/util/FunctionData.py
+++ defi_protocols/util/FunctionData.py
@@ -1,17 +1,15 @@
 from typing import Optional, Union
 from dataclasses import dataclass
 from time import sleep
+import json
 
-from web3 import Web3, exceptions
-from eth_account import Account
-from ape import accounts
-from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
+from web3 import Web3
+from web3._utils.abi import get_abi_input_types, filter_by_name
+from eth_abi import encode_abi
 
 from defi_protocols.util.api import RequestFromScan
 from defi_protocols.constants import SAFETRANSACTIONWITHROLE
 
-accountOne = accounts.load('revoker-1')
 
 @dataclass
 class ContractFunction:
@@ -37,65 +35,12 @@ class ContractFunction:
         return result
 
     def data_input(self) -> str:
-        result = self.function_instance(*self.function_args).build_transaction()['data']
+        name = filter_by_name(self.function_name,json.loads(self.contract_abi))[0]
+        types = get_abi_input_types(name)
+        signature = Web3.keccak(text=f"{self.function_name}({','.join(types)})").hex()[:10]
+        result = f"{signature}{encode_abi(types, self.function_args).hex()}"
         return result
 
-    def transact_with_role_from_EOA(
-        self,
-        private_key: str = None,
-        chain_id: Optional[int] = None,
-        gas_limit: Optional[int] = None,
-        max_fee_per_gas: Optional[int] = None,
-        max_prio_fee_per_gas: Optional[int] = None,
-        nonce: Optional[int] = None,
-        ) -> str:
-        from_address = Account.from_key(private_key)
-        if not max_fee_per_gas:
-            max_fee_per_gas = self.web3.to_wei('5', 'gwei')
-
-        if not nonce:
-            nonce = self.web3.eth.get_transaction_count(from_address.address)
-        if not max_prio_fee_per_gas:
-            max_prio_fee_per_gas = self.web3.eth.max_priority_fee
 
 
-        chain_id = self.web3.eth.chain_id        
-        tx = self.function_instance(*self.function_args).build_transaction({
-            'chainId':chain_id,
-            'gas': 500000,
-            'maxFeePerGas': max_fee_per_gas,
-            'maxPriorityFeePerGas': max_prio_fee_per_gas,
-            'nonce': nonce,
-        })
-        # if not gas_limit:
-        #     tx['gas'] = self.web3.eth.estimate_gas({'to': self.contract_address, 'data':self.function_instance(*self.function_args).build_transaction()['data']}) * 1.1
-        # else:
-        #     tx['gas'] = gas_limit
-        # print(gas_limit)
-        #print(tx)
-        signed_txn = self.web3.eth.account.sign_transaction(tx, private_key)
-        #print(tx['gas'])
-        executed_txn = self.web3.eth.send_raw_transaction(signed_txn.rawTransaction)
-
-        return executed_txn.hex()
-    
-    def get_tx_receipt(self,tx_hash):
-        try:
-            transaction_receipt = self.web3.eth.wait_for_transaction_receipt(tx_hash)
-            return  transaction_receipt
-        except exceptions.TransactionNotFound:
-            return 'Transaction not yet on blockchain'
-    
-    
-    def send_transaction_receipt_to_slack(self,channel,message):
-        slack_client = WebClient(token=SAFETRANSACTIONWITHROLE)
-        try:
-            response = slack_client.chat_postMessage(
-                channel=channel,
-                text=message
-            )
-            print(f"Message sent successfully: {response['ts']}")
-        except SlackApiError as e:
-            print(f"Error sending message: {e}")
-
-        
+       
